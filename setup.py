ab394e313a04892910a34b2b28cf93e8ecab61b7
George Austin <gia2105@columbia.edu>
2025-09-13 12:47:51 -0400

    adding multiclass rloocv

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='RebalancedCV',
-    version='0.0.1',
+    version='0.1.0',
     author='George Austin', 
     author_email='gia2105@columbia.edu', 
     url='https://github.com/korem-lab/RebalancedCV',
